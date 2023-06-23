9514e0abaa2931830d644a89829f1b61b9abcf50
sarmadmehmood89 <107080349+sarmadmehmood89@users.noreply.github.com>
2023-06-23 14:42:43 -0500

    Update microhomology_analysis.py

--- microhomology_analysis.py
+++ microhomology_analysis.py
@@ -2,60 +2,56 @@ import pysam
 import matplotlib.pyplot as plt
 import sys
 
-# Function to calculate microhomology
-def calculate_microhomology(sequence, k):
-    left_end = sequence[:k]  # k bases from the left end
-    right_end = sequence[-k:]  # k bases from the right end
+def calculate_microhomology(seq1, seq2):
+    """Checks for microhomology between two sequences"""
+    min_length = min(len(seq1), len(seq2))
+    for k in range(min(15, min_length), 1, -1):
+        if seq1[:k] == seq2[:k]:
+            return True
+    return False
 
-    # Compare the nucleotides at each position and count microhomology
-    microhomology_count = sum(1 for a, b in zip(left_end, right_end) if a == b)
+def get_sequences(genome, chr_name, start, end):
+    """Fetches upstream and downstream sequences for a given genomic region"""
+    upstream_start = genome.fetch(chr_name, start-15, start)
+    upstream_end = genome.fetch(chr_name, end-15, end)
+    downstream_start = genome.fetch(chr_name, start, start+15)
+    downstream_end = genome.fetch(chr_name, end, end+15)
+    return upstream_start, upstream_end, downstream_start, downstream_end
 
-    return microhomology_count > 0
+def main(genome_file, eccdna_file):
+    genome = pysam.FastaFile(genome_file)
 
-# Input files from command line arguments
-genome_file = sys.argv[1] 
-eccdna_file = sys.argv[2]
+    with open(eccdna_file, "r") as file:
+        lines = file.readlines()
 
-# Open the genome file
-genome = pysam.FastaFile(genome_file)
+    with_microhomology, without_microhomology = 0, 0
 
-# Open the eccDNA file
-with open(eccdna_file, "r") as file:
-    lines = file.readlines()
+    for line in lines:
+        chr_name, start, end, _ = line.strip().split()
+        start, end = int(start), int(end)
 
-# Counters for eccDNA regions with and without microhomology
-with_microhomology = 0
-without_microhomology = 0
+        upstream_start, upstream_end, downstream_start, downstream_end = get_sequences(genome, chr_name, start, end)
 
-# Process each line in the eccDNA file
-for line in lines:
-    chr_name, start, end, _ = line.strip().split()
-    start, end = int(start), int(end)
+        if calculate_microhomology(upstream_start, upstream_end) or calculate_microhomology(downstream_start, downstream_end):
+            with_microhomology += 1
+        else:
+            without_microhomology += 1
 
-    # Fetch the sequence from the genome
-    sequence = genome.fetch(chr_name, start, end)
+    total_eccDNA = with_microhomology + without_microhomology
 
-    # Check for microhomology
-    if calculate_microhomology(sequence, k=15):  # Here, k is set to 15
-        with_microhomology += 1
-    else:
-        without_microhomology += 1
+    with_microhomology_percentage = (with_microhomology / total_eccDNA) * 100
+    without_microhomology_percentage = (without_microhomology / total_eccDNA) * 100
 
-total_eccDNA = with_microhomology + without_microhomology
+    plt.bar(['With microhomology', 'Without microhomology'],
+            [with_microhomology_percentage, without_microhomology_percentage], color='b')
 
-# Calculate percentages
-with_microhomology_percentage = (with_microhomology / total_eccDNA) * 100
-without_microhomology_percentage = (without_microhomology / total_eccDNA) * 100
+    plt.xlabel('Category')
+    plt.ylabel('Percentage')  
+    plt.title('Microhomology Analysis')
+    plt.savefig('microhomology_plot.png')
+    plt.show()
 
-# Plot the percentages
-plt.bar(['With microhomology', 'Without microhomology'],
-        [with_microhomology_percentage, without_microhomology_percentage], color='b')
-
-plt.xlabel('Category')
-plt.ylabel('Percentage')  # y-axis label is now 'Percentage'
-plt.title('Microhomology Analysis')
-
-# Save the plot to a file before showing it
-plt.savefig('microhomology_plot.png')
-
-plt.show()
+if __name__ == "__main__":
+    genome_file = sys.argv[1]
+    eccdna_file = sys.argv[2]
+    main(genome_file, eccdna_file)
